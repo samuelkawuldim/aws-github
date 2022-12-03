684241614c8946e27cbf6ee6fef60e72b05fa716
samuelkawuldim <kawuldimsamuel@yahoo.com>
2022-12-02 20:57:16 -0600

    modified application

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
      <h1>Welcome to compudemy</h1>
      <p>my name is samuel
-      I am a Devops Engineer by profeesion with over two years of experirnce designing applications
+      I am a Devops Engineer by profeesion with over two years of experirnce designing applications. i will be happy to work with u
       </p>
       </div>
   );
